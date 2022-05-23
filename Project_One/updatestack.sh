9ba972524bc216e23bf553a3f47854b111aa0707
Micah Shallom <micahshallom@gmail.com>
2022-05-23 02:21:09 +0100

    finished networking segment

--- Project_One/updatestack.sh
+++ Project_One/updatestack.sh
@@ -1,1 +1,1 @@
-aws cloudformation update-stack --stack-name $1 --region=us-east-1 --template-body --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"
+aws cloudformation update-stack --stack-name $1 --region=us-east-1 --template-body file://$2 --parameters file://$3 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"
